0c883f1e0c6eab11f70fe698ef14145d07575a1c
michaellyon <michaellyon@localhost.localdomain>
2021-04-29 13:26:23 +0100

    Passwd change

--- wallet_validate/settings.py
+++ wallet_validate/settings.py
@@ -130,7 +130,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jjamst11@gmail.com'
-EMAIL_HOST_PASSWORD = 'oyostate'        
+EMAIL_HOST_PASSWORD = 'oyostate11'        
 
 
 STATIC_ROOT  =  os.path.join(BASE_DIR, 'staticfiles')
